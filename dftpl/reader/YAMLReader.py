b6b421b9b6cbab2eb2f05d544b2bab7c61e90702
Hudan Studiawan <studiawan@gmail.com>
2024-06-29 21:30:17 +0700

    Add YAML reader

---
+++ dftpl/reader/YAMLReader.py
@@ -0,0 +1,9 @@
+import yaml
+
+class YAMLReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read(self):
+        with open(self.file_path, 'r') as file:
+            return yaml.safe_load(file)
