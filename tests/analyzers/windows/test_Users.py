f8091e997b5597ced28e6d30818da59bbcc548ad
Hudan Studiawan <studiawan@gmail.com>
2024-07-07 10:19:10 +0700

    Add unit test for CreatedUser event

---
+++ tests/analyzers/windows/test_Users.py
@@ -0,0 +1,9 @@
+import pytest
+from dftpl.timelines.LowLevelTimeline import LowLevelTimeline
+from dftpl.events.LowLevelEvent import LowLevelEvent
+from dftpl.analyzers.windows.Users import CreatedUser
+
+
+@pytest.fixture
+def low_timeline():
+    pass
