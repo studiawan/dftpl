2d40026927bd87bfda059eca2fa93e6721b83dff
Hudan Studiawan <studiawan@gmail.com>
2024-07-07 10:19:32 +0700

    Add unit test for TimezoneSettings

---
+++ tests/analyzers/windows/test_TimezoneSettings.py
@@ -0,0 +1,9 @@
+import pytest
+from dftpl.timelines.LowLevelTimeline import LowLevelTimeline
+from dftpl.events.LowLevelEvent import LowLevelEvent
+from dftpl.analyzers.windows.TimezoneSettings import TimezoneSettings
+
+
+@pytest.fixture
+def low_timeline():
+    pass
