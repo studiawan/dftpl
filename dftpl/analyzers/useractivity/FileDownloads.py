d18df00cd59ea788a350aabd99044ea59f653b2f
Hudan Studiawan <studiawan@gmail.com>
2024-07-10 06:37:13 +0700

    Add analyzer for file downloads

--- dftpl/analyzers/useractivity/FileDownloads.py
+++ dftpl/analyzers/useractivity/FileDownloads.py
@@ -1,6 +1,8 @@
-__author__ = 'Chris Hargreaves'
+__author__ = 'Chris Hargreaves, Hudan Studiawan'
 
 import re
+import urllib.parse
+from datetime import datetime, timedelta
 from dftpl.timelines.HighLevelTimeline import HighLevelTimeline
 from dftpl.events.LowLevelEvent import LowLevelEvent
 from dftpl.events.HighLevelEvent import HighLevelEvent, ReasoningArtefact
@@ -21,74 +23,88 @@ def FileDownloaded(low_timeline, start_id, end_id):
 
     # Create a test event to match against
     test_event = LowLevelEvent()
-    test_event.type = "Created|c_time"
-    test_event.path = ".*/Users/(.*)/Downloads/(.*)"
+    test_event.type = "Creation Time-FILE"
+    test_event.evidence = r'\\Users\\[^\\]+\\Downloads'
 
     # Create a high level timeline to store the results
     high_level_timeline = HighLevelTimeline()
 
     # Find matching events
-    trigger_matches = low_timeline.find_matching_events_in_id_range(start_id,end_id, test_event)
+    trigger_matches = low_timeline.find_matching_events_in_id_range(start_id, end_id, test_event)
 
     # Extract details from matching events
     for each_event in trigger_matches:
         if each_event.match(test_event):
             # Create a high level event
             high_event = HighLevelEvent()
+            high_event.id = each_event.id
             high_event.add_time(each_event.date_time_min)
             high_event.evidence_source = each_event.evidence
             high_event.type = "File Downloaded"
-            file_name = re.search(".*/Users/(.*)/Downloads/(.*)", each_event.path).group(2)
-            user = re.search(".*/Users/(.*)/Downloads/(.*)", each_event.path).group(1)
-            high_event.AddFile(each_event.path)
-            high_event.set_keys("File Name", file_name )
-            high_event.set_keys("User", user)
+            file_name = re.search(r'\\Downloads\\([^\\]+\.[^\\\s]+)', each_event.evidence).group(1)
+            user = re.search(r'\\Users\\([^\\]+)\\Downloads', each_event.evidence).group(1)
+            
             high_event.description = "File Downloaded (%s)" % file_name
             high_event.category = analyser_category
-            high_event.device = each_event.evidence
+            high_event.device = each_event.plugin
+            high_event.files = each_event.path
+            high_event.set_keys("File Name", file_name )
+            high_event.set_keys("User", user)
 
             # Create a reasoning artefact
             reasoning = ReasoningArtefact()
             reasoning.id = each_event.id
-            reasoning.description = "Created time for file in downloads folder "
+            reasoning.description = f"Created time for file in downloads folder {','.join(each_event.provenance['raw_entry'])}"
             reasoning.test_event = test_event
 
             # Add the reasoning artefact to the high level event
             high_event.trigger = reasoning
+            
+            # pattern for test event
+            url_pattern = r"https?:\/\/[^\s]+\/"
+
+            # Define the second regex pattern (could be user input or another variable)
+            encoded_string = urllib.parse.quote(file_name)
+            file_pattern = re.escape(encoded_string)
+
+            # Combine the two patterns to ensure the match includes the file name
+            combined_pattern = fr"{url_pattern}{file_pattern}"
 
             # Test event for source URL
             source_url_test_event = LowLevelEvent()
-            source_url_test_event.type = "URL Visit"
-            source_url_test_event.path = "{}$".format(file_name)
+            source_url_test_event.type = "End Time-WEBHIST"
+            source_url_test_event.evidence = combined_pattern
 
-            # Find matching events
-            results = low_timeline.get_list_of_matches_in_sub_timeline({'source_url':source_url_test_event},start=each_event.date_time_min-15, end=each_event.date_time_min+15)
+            # Find matching events to find source URL
+            fuzzy_period = timedelta(seconds=180)
+            start_time = datetime.fromisoformat(each_event.date_time_min) - fuzzy_period
+            end_time = datetime.fromisoformat(each_event.date_time_min) + fuzzy_period
+
+            # Find source URL
+            results = low_timeline.get_list_of_matches_in_sub_timeline(source_url_test_event, start=start_time, end=end_time)
 
             if results:
-                for each_match in results:
-                    each_match_event = each_match[0] # the event
-                    each_match_type = each_match[1] # source_url in this case
+                supporting = []
+                for result in results:
+                    # Create a reasoning artefact for source URL
+                    supporting_event = ReasoningArtefact()
+                    supporting_event.id = result.id
+                    supporting_event.description = "File Downloaded ({}) from {}".format(file_name, GetURIStringFromMessage(result.evidence))
+                    supporting_event.date_time_min = result.date_time_min
+                    supporting_event.test_event = source_url_test_event
+                    supporting.append(supporting_event)
 
-                    #update times for additional matched event
-                    high_event.add_time(each_match_event.date_time_min)
-                    high_event.add_time(each_match_event.date_time_max)
-
-                    # update description
-                    high_event.description = "File Downloaded ({}) from ({})".format(file_name, each_match_event.path)
-
-                    #add as supporting artefacts
-                    supporting = ReasoningArtefact()
-                    supporting.description = "Found URL containing downloaded filename (%s)" % (each_match_event.path)
-                    supporting.id = each_match_event.id
-                    supporting.test_event = source_url_test_event
-                    high_event.AddSupportingEvidenceArtefact(supporting)
-            else:
-                    #add as contradictory artefacts
-                    contradictory = ReasoningArtefact()
-                    contradictory.description = "Did not find URL containing downloaded filename"
-                    contradictory.id = -1
-                    contradictory.test_event = source_url_test_event
-                    high_event.AddContradictoryEvidenceArtefact(contradictory)
-
+                high_event.supporting = supporting
 
             high_level_timeline.add_event(high_event)
+    
+    return high_level_timeline
+
+
+def GetURIStringFromMessage(message):
+    """Extracts the URI from a message"""
+    uri = re.search(r"https?:\/\/[^\s]+\/", message)
+    if uri:
+        return uri.group(0)
+    else:
+        return None
---
+++ tests/analyzers/useractivity/test_FileDownloads.py
@@ -0,0 +1,95 @@
+import pytest
+from dftpl.timelines.LowLevelTimeline import LowLevelTimeline
+from dftpl.events.LowLevelEvent import LowLevelEvent
+from dftpl.analyzers.useractivity.FileDownloads import FileDownloaded
+
+
+# file download
+# 2023-12-26 00:37:07.947361+00:00,Creation Time,FILE,File stat,NTFS:\Users\User\Downloads\Firefox Installer.exe Type: file,filestat,NTFS:\Users\User\Downloads\Firefox Installer.exe,-
+
+# supporting 
+# 2023-12-26 00:37:23.781311+00:00,End Time,WEBHIST,Chrome History,https://cdn.stubdownloader.services.mozilla.com/builds/firefox-stub/en-US/win/ef5a2910edab879b9d22df3dbd331af666fa69b040641e1e053b2ea6ca113d26/Firefox%20Installer.exe (C:\Users\User\Downloads\Firefox Installer.exe). State: Complete. Received 349976 of 349976 bytes. Interrupt Reason: No Interrupt - Success. Danger Type: Content May Be Malicious - (eg: extension is exe but Safe Browsing has not finished checking the content).,sqlite/chrome_27_history,NTFS:\Users\User\AppData\Local\Microsoft\Edge\User Data\Default\History,-
+
+@pytest.fixture
+def low_timeline():
+    # Create a test event to match against
+    event1 = LowLevelEvent()
+    event1.id = 1
+    event1.date_time_min = "2023-12-26 00:37:07.947361+00:00"
+    event1.date_time_max = None
+    event1.type = "Creation Time-FILE"
+    event1.path = r"NTFS:\Users\User\Downloads\Firefox Installer.exe"
+    event1.evidence = r"NTFS:\Users\User\Downloads\Firefox Installer.exe Type: file"
+    event1.plugin = "FILE-File stat-filestat"
+    event1.provenance = {
+        'line_number': 1,
+        'raw_entry': ["2023-12-26 00:37:07.947361+00:00",
+                      "Creation Time",
+                      "FILE",
+                      "File stat",
+                      r"NTFS:\Users\User\Downloads\Firefox Installer.exe Type: file",
+                      "filestat",
+                      r"NTFS:\Users\User\Downloads\Firefox Installer.exe",
+                      "-"]
+    }
+    event1.keys = None
+
+    event2 = LowLevelEvent()
+    event2.id = 2
+    event2.date_time_min = "2023-12-26 00:37:23.781311+00:00"
+    event2.date_time_max = None
+    event2.type = "End Time-WEBHIST"
+    event2.path = r"NTFS:\Users\User\AppData\Local\Microsoft\Edge\User Data\Default\History"
+    event2.evidence = r"https://cdn.stubdownloader.services.mozilla.com/builds/firefox-stub/en-US/win/ef5a2910edab879b9d22df3dbd331af666fa69b040641e1e053b2ea6ca113d26/Firefox%20Installer.exe (C:\Users\User\Downloads\Firefox Installer.exe). State: Complete. Received 349976 of 349976 bytes. Interrupt Reason: No Interrupt - Success. Danger Type: Content May Be Malicious - (eg: extension is exe but Safe Browsing has not finished checking the content)."
+    event2.plugin = "WEBHIST-Chrome History-sqlite/chrome_27_history"
+    event2.provenance = {
+        'line_number': 2,
+        'raw_entry': ["2023-12-26 00:37:23.781311+00:00",
+                      "End Time",
+                      "WEBHIST",
+                      "Chrome History",
+                      r"https://cdn.stubdownloader.services.mozilla.com/builds/firefox-stub/en-US/win/ef5a2910edab879b9d22df3dbd331af666fa69b040641e1e053b2ea6ca113d26/Firefox%20Installer.exe (C:\Users\User\Downloads\Firefox Installer.exe). State: Complete. Received 349976 of 349976 bytes. Interrupt Reason: No Interrupt - Success. Danger Type: Content May Be Malicious - (eg: extension is exe but Safe Browsing has not finished checking the content).",
+                      "sqlite/chrome_27_history",
+                      r"NTFS:\Users\User\AppData\Local\Microsoft\Edge\User Data\Default\History",
+                      "-"]
+    }
+    event2.keys = None
+
+    # Add events to timeline
+    timeline = LowLevelTimeline()
+    timeline.add_event(event1)
+    timeline.add_event(event2)
+
+    return timeline
+
+def test_FileDownloads(low_timeline):
+    # file download
+    start_id = 0
+    end_id = 2
+    high_timeline = FileDownloaded(low_timeline, start_id, end_id)
+
+    # high level event
+    assert len(high_timeline.events) == 1
+    assert high_timeline.events[0].id == 1
+    assert high_timeline.events[0].date_time_min == "2023-12-26 00:37:07.947361+00:00"
+    assert high_timeline.events[0].date_time_max == "2023-12-26 00:37:07.947361+00:00"
+    assert high_timeline.events[0].evidence_source == r"NTFS:\Users\User\Downloads\Firefox Installer.exe Type: file"
+    assert high_timeline.events[0].type == "File Downloaded"
+    assert high_timeline.events[0].description == "File Downloaded (Firefox Installer.exe)"
+    assert high_timeline.events[0].category == "User Activity"
+    assert high_timeline.events[0].device == "FILE-File stat-filestat"
+    assert high_timeline.events[0].files == r"NTFS:\Users\User\Downloads\Firefox Installer.exe"
+    assert high_timeline.events[0].keys['File Name'] == "Firefox Installer.exe"
+    assert high_timeline.events[0].keys['User'] == "User"
+
+    # reasoning
+    assert high_timeline.events[0].trigger.id == 1
+    assert high_timeline.events[0].trigger.description == "Created time for file in downloads folder 2023-12-26 00:37:07.947361+00:00,Creation Time,FILE,File stat,NTFS:\\Users\\User\\Downloads\\Firefox Installer.exe Type: file,filestat,NTFS:\\Users\\User\\Downloads\\Firefox Installer.exe,-"
+
+    # supporting events
+    assert len(high_timeline.events[0].supporting) == 1
+    assert high_timeline.events[0].supporting[0].id == 2
+    assert high_timeline.events[0].supporting[0].description == "File Downloaded (Firefox Installer.exe) from https://cdn.stubdownloader.services.mozilla.com/builds/firefox-stub/en-US/win/ef5a2910edab879b9d22df3dbd331af666fa69b040641e1e053b2ea6ca113d26/"
+    assert high_timeline.events[0].supporting[0].date_time_min == "2023-12-26 00:37:23.781311+00:00"
+    assert high_timeline.events[0].supporting[0].test_event.type == "End Time-WEBHIST"
+
