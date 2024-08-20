5d2f6a31fe348b042c7e2934e9439dda38e6784b
Hudan Studiawan <studiawan@gmail.com>
2024-08-21 04:00:07 +0700

    Update recent file access analyzer

--- dftpl/analyzers/useractivity/RecentFileAccess.py
+++ dftpl/analyzers/useractivity/RecentFileAccess.py
@@ -1,5 +1,6 @@
 import re
 from dftpl.timelines.HighLevelTimeline import HighLevelTimeline
+from dftpl.timelines.LowLevelTimeline import LowLevelTimeline
 from dftpl.events.LowLevelEvent import LowLevelEvent
 from dftpl.events.HighLevelEvent import HighLevelEvent, ReasoningArtefact
 
@@ -7,14 +8,14 @@ from dftpl.events.HighLevelEvent import HighLevelEvent, ReasoningArtefact
 description = "Recent File Access"
 analyser_category = "User Activity"
 
-def Run(timeline, start_id=0, end_id=None):
+def Run(timeline: LowLevelTimeline, start_id: int=0, end_id=None) -> HighLevelTimeline:
     """Runs the Recent File Access analyser"""
     if end_id == None:
-        end_id = len(timeline)
+        end_id = len(timeline.events)
 
     return RecentFileAccess(timeline, start_id, end_id)
 
-def RecentFileAccess(low_timeline, start_id, end_id):
+def RecentFileAccess(low_timeline: LowLevelTimeline, start_id: int, end_id: int) -> HighLevelTimeline:
     """Finds recent file accesses"""
     
     # Get the test event dictionary
@@ -26,43 +27,45 @@ def RecentFileAccess(low_timeline, start_id, end_id):
     # Find matching events
     trigger_matches = low_timeline.find_matching_events_with_test_event_dict(test_event_dict, start_id, end_id)
 
-    for each_event in trigger_matches:
-        # create high level event
-        high_event = HighLevelEvent()
-        high_event.id = each_event.id
-        high_event.add_time(each_event.date_time_min)
-        high_event.evidence_source = each_event.evidence
-        high_event.type = "Recent File Access"
-        high_event.description = description
-        high_event.category = analyser_category
-        high_event.device = each_event.plugin
-        high_event.files = each_event.path
-        high_event.supporting = low_timeline.get_supporting_events(each_event.id)
+    if trigger_matches:
+        for each_event in trigger_matches:
+            # create high level event
+            high_event = HighLevelEvent()
+            high_event.id = each_event.id
+            high_event.add_time(each_event.date_time_min)
+            high_event.evidence_source = each_event.evidence
+            high_event.type = "Recent File Access"
+            high_event.description = description
+            high_event.category = analyser_category
+            high_event.device = each_event.plugin
+            high_event.files = each_event.path
+            high_event.supporting = low_timeline.get_supporting_events(each_event.id)
 
-        # get file details from trigger event
-        file_details = GetMatchedFileDetailsFromTrigger(each_event)
-        if file_details['file_details_from_lnk']:
-            high_event.set_keys("file_details_from_lnk", file_details['file_details_from_lnk'])
+            # get file details from trigger event
+            file_details = GetMatchedFileDetailsFromTrigger(each_event)
+            if file_details['file_details_from_lnk']:
+                high_event.set_keys("file_details_from_lnk", file_details['file_details_from_lnk'])
 
-        if file_details['file_details_from_entry_shell']:    
-            high_event.set_keys("file_details_from_entry_shell", file_details['file_details_from_entry_shell'])
+            if file_details['file_details_from_entry_shell']:    
+                high_event.set_keys("file_details_from_entry_shell", file_details['file_details_from_entry_shell'])
 
-        # create reasoning artefact
-        reasoning = ReasoningArtefact()
-        reasoning.id = each_event.id
-        reasoning.description = f"Recent file access {','.join(each_event.provenance['raw_entry'])}"
-        reasoning.test_event = test_event_dict
+            # create reasoning artefact
+            reasoning = ReasoningArtefact()
+            reasoning.id = each_event.id
+            reasoning.description = f"Recent file access: {each_event.path}"
+            reasoning.test_event = ConvertTestEventToDict(test_event_dict) 
+            reasoning.provenance = each_event.provenance
 
-        # Add the reasoning artefact to the high level event
-        high_event.trigger = reasoning
-        
-        # Add the high level event to the timeline
-        high_timeline.add_event(high_event)
+            # Add the reasoning artefact to the high level event
+            high_event.trigger = reasoning.to_dict()
+            
+            # Add the high level event to the timeline
+            high_timeline.add_event(high_event)
 
-    return high_timeline
+        return high_timeline
 
 
-def GetTestEventDictionary():
+def GetTestEventDictionary() -> dict:
     """Returns list of test events"""
 
     # Windows shortcut or lnk file
@@ -103,7 +106,26 @@ def GetTestEventDictionary():
 
     return test_event_dict
 
-def GetMatchedFileDetailsFromTrigger(event):
+def ConvertTestEventToDict(test_event: dict) -> dict:
+    """Converts all test event dictionary elements to a dictionary"""
+    test_event_dict = {
+        "recent_from_lnk": {
+            "plugin": test_event["recent_from_lnk"].plugin,
+            "type": test_event["recent_from_lnk"].type,
+            "evidence": test_event["recent_from_lnk"].evidence,
+            "path": test_event["recent_from_lnk"].path
+        },
+        "recent_from_entry_shell": {
+            "plugin": test_event["recent_from_entry_shell"].plugin,
+            "type": test_event["recent_from_entry_shell"].type,
+            "evidence": test_event["recent_from_entry_shell"].evidence,
+            "path": test_event["recent_from_entry_shell"].path
+        }
+    }
+
+    return test_event_dict
+
+def GetMatchedFileDetailsFromTrigger(event: LowLevelEvent) -> dict:
     """Extracts file details from the trigger event"""
     file_details_dict = {
         'file_details_from_lnk': GetMatchedFileDetailsFromLNK(event.evidence),
@@ -113,7 +135,7 @@ def GetMatchedFileDetailsFromTrigger(event):
     # return the extracted values
     return file_details_dict
 
-def GetMatchedFileDetailsFromLNK(event):
+def GetMatchedFileDetailsFromLNK(event: LowLevelEvent) -> dict:
     """Extracts file details from the trigger event"""
     file_details_dict = {}
 
@@ -138,10 +160,9 @@ def GetMatchedFileDetailsFromLNK(event):
             file_details_dict[key] = match.group(1).rstrip()
 
     # return the extracted values
-    print(file_details_dict)
     return file_details_dict
 
-def GetMatchedFileDetailsFromEntryShell(event):
+def GetMatchedFileDetailsFromEntryShell(event: LowLevelEvent) -> dict:
     """Extracts file details from the entry shell event"""
     file_details_dict = {}
 
