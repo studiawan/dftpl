03ce04abfa54c1b39fd69c6998c13d04ab7e0c73
Hudan Studiawan <studiawan@gmail.com>
2024-08-21 05:49:59 +0700

    Add to_dict in ReasoningArtifact class

--- dftpl/events/HighLevelEvent.py
+++ dftpl/events/HighLevelEvent.py
@@ -1,4 +1,8 @@
+from typing import Any
+
+
 class HighLevelEvent:
+    """High level event class"""
     def __init__(self):
         self.id = None              # Unique identifier for the event
         self.date_time_min = None   # Earliest time the event could have occurred
@@ -13,22 +17,24 @@ class HighLevelEvent:
         self.trigger = None         # Reasoning artefact that triggered the event
         self.supporting = {}        # List of reasoning artefacts supporting the event, five low level events before and after the event
         self.merged_id = []         # List of IDs of events that have been merged into this event
+        self.date_time_iso = None   # ISO 8601 formatted date time
 
-    def add_time(self, date_time):
+    def add_time(self, date_time: str):
         # Sets the time for the event, adjusting min and max if necessary
         self.date_time_min = date_time
         self.date_time_max = date_time
 
-    def set_keys(self, key, value):
+    def set_keys(self, key: Any, value: Any):
         # Adds additional information to the event
         self.keys[key] = value
     
-    def merge(self, event_id):
+    def merge(self, event_id: int):
         # Adds an event ID to the list of merged events
         self.merged_id.append(event_id)
 
 
 class ReasoningArtefact:
+    """Reasoning artefact class"""
     def __init__(self):
         self.id = None              # Unique identifier for the reasoning artefact
         self.description = None     # Human-readable description of the reasoning artefact
@@ -37,11 +43,32 @@ class ReasoningArtefact:
         self.keys = {}              # Additional key-value pairs with extra information
         self.references = None      # Reference to external sources
 
-    def set_keys(self, key, value):
+    def set_keys(self, key: Any, value: Any):
         # Adds additional information to the reasoning artefact
         self.keys[key] = value
     
-    def add_time(self, date_time):
+    def add_time(self, date_time: str):
         # Sets the time for the event, adjusting min and max if necessary
         self.date_time_min = date_time
         self.date_time_max = date_time
+    
+    def to_dict(self) -> dict:
+        # Converts the reasoning artefact to a dictionary
+        if type(self.test_event) is not dict:
+            test_event = {
+                'type': self.test_event.type,
+                'evidence': self.test_event.evidence
+            },
+        else:
+            test_event = self.test_event
+        
+        reasoning_dict = {
+            'id': self.id,
+            'description': self.description,
+            'test_event': test_event,
+            'provenance': self.provenance,
+            'keys': self.keys,
+            'references': self.references
+        }
+        
+        return reasoning_dict
