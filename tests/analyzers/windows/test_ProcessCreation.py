77b1a2375c68630b44129d41f5a7c0c6f2ab8816
Hudan Studiawan <studiawan@gmail.com>
2024-08-20 11:37:24 +0700

    Update process creation analyzer

--- tests/analyzers/windows/test_ProcessCreation.py
+++ tests/analyzers/windows/test_ProcessCreation.py
@@ -35,7 +35,7 @@ def low_timeline():
 
 def test_FindProcessCreation(low_timeline):
     start_id = 0
-    end_id = 1
+    end_id = 2
     high_timeline = FindProcessCreation(low_timeline, start_id, end_id)
 
     assert len(high_timeline.events) == 1
@@ -48,19 +48,18 @@ def test_FindProcessCreation(low_timeline):
     assert high_timeline.events[0].keys["Executable name"] == "msedge.exe"
     assert high_timeline.events[0].files == r"NTFS:\Windows\System32\winevt\Logs\Microsoft-Windows-Shell-Core%4Operational.evtx"
     assert high_timeline.events[0].supporting == {
-        'before': [{
-            'id': low_timeline.events[0].id,
-            'date_time_min': low_timeline.events[0].date_time_min,
-            'date_time_max': low_timeline.events[0].date_time_max,
-            'type': low_timeline.events[0].type,
-            'path': low_timeline.events[0].path,
-            'evidence': low_timeline.events[0].evidence,
-            'provenance': low_timeline.events[0].provenance,
-            'plugin': low_timeline.events[0].plugin,
-            'keys': low_timeline.events[0].keys
-        }],
+        'before': [],
         'after': [],
     }
 
-    assert high_timeline.events[0].reasoning.id == 1
-    assert high_timeline.events[0].reasoning.description == "Process creation event found in 2023-12-26 00:34:47.890403+00:00,Content Modification Time,EVT,WinEVTX,[9707 / 0x25eb] Provider identifier: {30336ed4-e327-447c-9de0-51b652c86108} Source Name: Microsoft-Windows-Shell-Core Strings: ['msedge.exe --no-startup-window --win-session-start'] Computer Name: WinDev2311Eval Record Number: 2249 Event Level: 4,winevtx,NTFS:\\Windows\\System32\\winevt\\Logs\\Microsoft-Windows-Shell-Core%4Operational.evtx,-"
+    assert high_timeline.events[0].trigger == {
+        'id': low_timeline.events[0].id,
+        'description': f"Process creation event of 'msedge.exe' found with Windows event ID 9707",
+        'test_event': {
+            'type': low_timeline.events[0].type,
+            'evidence': r'\[\s*(9707|0x25eb)\s*/\s*(9707|0x25eb)\s*\].*\'([^\']+\.exe)'
+        },
+        'provenance': low_timeline.events[0].provenance,
+        'references': 'https://github.com/Psmths/windows-forensic-artifacts/blob/main/execution/evtx-9707-shell-core.md',
+        'keys': {},
+    }
