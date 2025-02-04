419b7aca9d048ba2d73254d22e1f3aa855cfa158
Java Kanaya Prada <javakanaya@gmail.com>
2025-02-04 22:30:40 +0700

    feat: update YAML rule parsing and store it in Rule class

--- dftpl/reader/YAMLReader.py
+++ dftpl/reader/YAMLReader.py
@@ -1,9 +1,13 @@
 import yaml
+from dftpl.rules.Rule import Rule
+
 
 class YAMLReader:
     def __init__(self, file_path):
         self.file_path = file_path
 
-    def read(self):
-        with open(self.file_path, 'r') as file:
-            return yaml.safe_load(file)
+    def read(self) -> Rule:
+        with open(self.file_path, "r") as file:
+            rule_data = yaml.safe_load(file)
+
+        return Rule.from_yaml(rule_data)
